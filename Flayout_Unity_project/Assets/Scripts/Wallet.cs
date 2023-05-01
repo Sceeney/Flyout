225869d6f939c072da9acb0a7ee102506998a38f
Lakello <59009343+Lakello@users.noreply.github.com>
2023-05-01 17:12:55 +0700

    Save

--- Flayout_Unity_project/Assets/Scripts/Wallet.cs
+++ Flayout_Unity_project/Assets/Scripts/Wallet.cs
@@ -2,35 +2,64 @@ using UnityEngine;
 using UnityEngine.Events;
 using YG;
 
-public class Wallet : MonoBehaviour
+public class Wallet : MonoBehaviour, ISaver
 {
     [SerializeField] private MoneyView _moneyView;
     private int _money;
 
-    public int Money { get { return _money; } }
+    public int Money { get { return GetMoney(); } }
 
-    private UnityAction<int> _updateMoney;
+    [SerializeField] private UnityEvent<int> _updateMoney;
 
-    private void OnEnable() => _updateMoney += _moneyView.OnUpdateMoney;
-    private void OnDisable() => _updateMoney -= _moneyView.OnUpdateMoney;
+    private void OnEnable() => YandexGame.GetDataEvent += GetData;
+    private void OnDisable() => YandexGame.GetDataEvent -= GetData;
 
     private void Start()
     {
-        SetMoney(267324);
+        if (YandexGame.SDKEnabled == true)
+        {
+            GetData();
+        }
+    }
+
+    private void GetData()
+    {
         GetMoney();
     }
 
-    public int GetMoney()
+    private int GetMoney()
     {
+        //YandexGame.LoadLocal();
+
         _money = YandexGame.savesData.Money;
 
         return _money;
     }
 
-    public void SetMoney(int value)
+    public void OnPurchased(Car car)
+    {
+        int price = car.Price;
+
+        _money -= price;
+
+        SetMoney(_money);
+    }
+
+    private void SetMoney(int value)
     {
         YandexGame.savesData.Money = value;
 
-        _updateMoney.DynamicInvoke(value);
+        _updateMoney?.Invoke(value);
+
+        Save();
+    }
+
+    public void Save()
+    {
+        YandexGame.SaveLocal();
+
+#if !UNITY_EDITOR
+        YandexGame.SaveProgress();
+#endif
     }
 }
