d71213b636e285229eb7482948e4d80db4568449
Lakello <59009343+Lakello@users.noreply.github.com>
2023-06-16 15:06:52 +0700

    f

---
+++ Flayout_Unity_project/Assets/Scripts/ADController.cs
@@ -0,0 +1,11 @@
+using IJunior.TypedScenes;
+using UnityEngine;
+using YG;
+
+public class ADController : MonoBehaviour, ISceneLoadHandler<int>
+{
+    public void OnSceneLoaded(int argument)
+    {
+        throw new System.NotImplementedException();
+    }
+}
